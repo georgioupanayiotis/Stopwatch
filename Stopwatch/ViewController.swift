66897fe4b1287cca6e07283d7cf8c28811e06701
Panayiotis Georgiou <info@panayiotisgeorgiou.net>
2016-02-02 14:24:55 +0000

    My first iPhone app, a stopwatch..

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var time = 0
+
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    func increaseTimer(){
+        
+        time++
+        timerLabel.text = "\(time)"
+        //timerLabel.text = String (time)
+        //or         timerLabel.text = "\(time)"
+        
+    }
+    
+    @IBAction func ExitApp(sender: AnyObject) {
+        
+        exit(0)
+    }
+  
+    @IBAction func play2(sender: AnyObject) {
+         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+    }
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate() //breaking the timer every sec
+        
+        
+    }
+    @IBAction func reset(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        time = 0
+        
+        timerLabel.text = "0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
